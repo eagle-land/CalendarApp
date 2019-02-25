bdb0204f44f9bc3e7857655e40b33cd1e29a9ba6
Jacob Weidokal <jweidokal@yahoo.com>
2019-02-25 18:46:19 -0500

    Adding routes to about page and contact page

--- app/__init__.py
+++ app/__init__.py
@@ -34,4 +34,12 @@ def create_app(test_config=None):
     def landing():
         return render_template('index.html')
 
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
+
+    @app.route('/contact')
+    def contact():
+        return render_template('contact.html')
+
     return app
