c9f1c503fec05015ab2c3d4cfe42343f3e6819f9
Jacob Weidokal <jweidokal@yahoo.com>
2019-03-06 18:53:31 -0500

    created route that calls google calendar api

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
+from __future__ import print_function
 import os
-
 from flask import Flask, render_template
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 
 def create_app(test_config=None):
@@ -44,7 +52,59 @@ def create_app(test_config=None):
     
     @app.route('/test')
     def testapi():
-        #CALL API HERE
-        return #API DATA HERE
+
+        # CALL API HERE
+        print('entered the function')
+
+        """Shows basic usage of the Google Calendar API.
+        Prints the start and name of the next 10 events on the user's calendar.
+        """
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                print('before')
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server()
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        service = build('calendar', 'v3', credentials=creds)
+
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        events_result = service.events().list(calendarId='nba_5_%43leveland+%43avaliers#sports@group.v.calendar.google.com', timeMin=now,
+                                              maxResults=10, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+
+        events_string = ""
+
+        # Formatting data to be returned to html
+        for event in events:
+            events_string += event['summary'] + ' ' + event['start'].get('dateTime') + '<br />'
+
+        return events_string
+
+    @app.route('/callback/<calendar_data>')
+    def callback(calendar_data):
+        return calendar_data
 
     return app
