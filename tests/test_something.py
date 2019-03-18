bc0d074bb5aea14a2f4f1ffd6a5246eb1e4c36e1
sardonicphoenix <37968570+sardonicphoenix@users.noreply.github.com>
2019-03-18 17:42:17 -0400

    added a kind of test

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,1 +1,13 @@
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome()
+try:
+    browser.get("http://127.0.0.1:5000/")
+
+    loginLink = browser.findElement(By.linkText("Login"));
+    browser.close()
+
+finally:
+    browser.quit()
