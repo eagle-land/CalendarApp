1826f93c6150a78443c028be9ca76de1ad641409
bryceb96 <brycebenjamin412@gmail.com>
2019-04-20 16:04:33 -0400

    made test_get_calendar.py test, although tests do not work due to a runtime error

---
+++ tests/test_get_calendar.py
@@ -0,0 +1,56 @@
+import app.calendar as calendar
+
+def test1():
+    #Example calendar 1
+    user1events = [
+        calendar.Event("2019-04-15T11:00:00-04:00", "2019-04-15T11:50:00-04:00")
+    ]
+    calendar1 = calendar.Calendar(user1events)
+    start = "2019-04-15T00:00:00-04:00"
+    end = "2019-04-21T00:00:00-04:00"
+    timezone = "America/New York"
+    result = calendar.get_calendar(start, end, timezone)
+    #result_string = ""
+    #for event in result:
+       # result_string += event.starttime + ' - ' + event.endtime + '\n'
+    #print(result_string)
+    assert result == calendar1
+
+
+    #Example calendar 2
+    user2events = [
+        calendar.Event("2019-04-15T20:00:00-04:00", "2019-04-15T20:50:00-04:00")
+    ]
+    calendar2 = calendar.Calendar(user2events)
+    start = "2019-04-15T20:00:00-04:00"
+    end = "2019-04-15T20:50:00-04:00"
+    timezone = "England/London"
+    result = calendar.get_calendar(start, end, timezone)
+    #result_string = ""
+   # for event in result:
+     #   result_string += event.starttime + ' - ' + event.endtime + '\n'
+   # print(result_string)
+    assert result == calendar2
+
+
+def test2():
+    #Example calendar 1
+    user3events = [
+        calendar.Event("2019-04-15T22:00:00-04:00", "2019-04-15T22:50:00-04:00")
+    ]
+    calendar3 = calendar.Calendar(user3events)
+    start = "2019-04-15T22:00:00-04:00"
+    end = "2019-04-21T22:00:00-04:00"
+    timezone = "America/California"
+    result = calendar.get_calendar(start, end, timezone)
+    #result_string = ""
+    #for event in result:
+       # result_string += event.starttime + ' - ' + event.endtime + '\n'
+    #print(result_string)
+    assert result == calendar3
+
+
+if __name__ == '__main__':
+    test1()
+    print("Passed all tests.")
+
