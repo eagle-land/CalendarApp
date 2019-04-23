f7dd0ef7342405f2605b4a878c65cbc267d2425c
MA-39 <jyunie265@gmail.com>
2019-04-23 16:51:55 -0400

    added test3 to test_merge_calendars which compares overlapping events

--- tests/test_merge_calendars.py
+++ tests/test_merge_calendars.py
@@ -52,7 +52,39 @@ def test2():
     assert result == mergedcalendar
 
 
+def test3():
+    #Example calendar 1
+    user1events = [
+        calendar.Event("2019-05-15T13:00:00-04:00", "2019-05-15T17:00:00-04:00"), 
+        calendar.Event("2019-05-15T16:00:00-04:00", "2019-05-15T20:00:00-04:00")
+            # year-month-day(T)hours:minutes:seconds(-04 is subtracted from UTC this is EST
+    ]
+    calendar1 = calendar.Calendar(user1events) #put user1 events into calendar
+
+    #second user calendar events defined
+    user2events = [
+        calendar.Event("2019-05-15T9:00:00-04:00", "2019-05-15T10:00:00-04:00"),
+	    calendar.Event("2019-05-15T8:00:00-04:00", "2019-05-15T22:00:00-04:00")
+    ]
+    calendar2 = calendar.Calendar(user2events) #put user2 events into calendar
+
+    #array with correct variables for compare
+    mergedevents = [
+        calendar.Event("2019-05-15T13:00:00-04:00", "2019-05-15T17:00:00-04:00"),
+	    calendar.Event("2019-05-15T16:00:00-04:00", "2019-05-15T20:00:00-04:00"),
+
+	    calendar.Event("2019-05-15T9:00:00-04:00", "2019-05-15T10:00:00-04:00"),
+	    calendar.Event("2019-05-15T8:00:00-04:00", "2019-05-15T22:00:00-04:00")
+    ]
+    
+    mergedcalendar = calendar.Calendar(mergedevents)            #compare calendar variable
+    result = calendar.merge_calendars(calendar1, calendar2)     #test variables compare
+    assert result == mergedcalendar                             #assert will say whether the test failed or not
+
+
+
 if __name__ == '__main__':
     test1()
     test2()
+    test3()
     print("Passed all tests.")
