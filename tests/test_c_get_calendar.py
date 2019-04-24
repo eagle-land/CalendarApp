c6012649304274d6e43cb77e8a78e5d764c6fee7
Jared Anderson <anderson.jared.16@gmail.com>
2019-04-24 16:52:07 -0400

    Fixed test for get_calendar.

--- tests/test_c_get_calendar.py
+++ tests/test_c_get_calendar.py
@@ -1,52 +1,31 @@
 import app.calendar as calendar
 
-"""#USERIDS================================================================================================
-plug these in for userid...uhh...we need to know whats in these to test....may need to make a new account
-also ask permission
-"""
 
-"""#CURRENTLY----------------------DOES-------------------------NOT------------------------------WORK-----------------------------------------------
 def test1():
-    #Example calendar 1
-    user1events = [
-        # year-month-day(T)two digit hours: two digit minutes: two digit seconds(-04 is subtracted from UTC, this for example is EST eastern time)
-        calendar.Event("2019-04-15T11:00:00-04:00", "2019-04-15T11:50:00-04:00")
+    userid = "auth0|5cb50516ee4bd5113d54872b"
+    start = "2019-04-15T00:00:00-04:00"
+    end = "2019-04-21T00:00:00-04:00"
+    timezone = "America/New_York"
+
+    events = [
+        calendar.Event("2019-04-15T11:00:00-04:00", "2019-04-15T11:50:00-04:00"),
+        calendar.Event("2019-04-15T12:30:00-04:00", "2019-04-15T13:45:00-04:00"),
+        calendar.Event("2019-04-15T19:00:00-04:00", "2019-04-15T20:15:00-04:00"),
+        calendar.Event("2019-04-16T12:30:00-04:00", "2019-04-16T13:45:00-04:00"),
+        calendar.Event("2019-04-16T20:00:00-04:00", "2019-04-16T22:00:00-04:00"),
+        calendar.Event("2019-04-17T11:00:00-04:00", "2019-04-17T11:50:00-04:00"),
+        calendar.Event("2019-04-17T12:30:00-04:00", "2019-04-17T13:45:00-04:00"),
+        calendar.Event("2019-04-17T19:00:00-04:00", "2019-04-17T20:15:00-04:00"),
+        calendar.Event("2019-04-18T12:30:00-04:00", "2019-04-18T13:45:00-04:00"),
+        calendar.Event("2019-04-18T15:30:00-04:00", "2019-04-18T16:00:00-04:00"),
+        calendar.Event("2019-04-19T11:00:00-04:00", "2019-04-19T11:50:00-04:00")
     ]
-    calendar1 = calendar.Calendar(user1events)
-    start = "2019-04-15T00:00:00-04:00"
-    end = "2019-04-21T00:00:00-04:00"
-    timezone = "America/New York" #keep this the same...we can't test other timezones rn
-    userid1 = "123" #use one of the above for userid
+    correct_calendar = calendar.Calendar(events)
 
-    result = calendar.get_calendar(userid1, start, end, timezone)
-    #result_string = ""
-    #for event in result:
-       # result_string += event.starttime + ' - ' + event.endtime + '\n'
-    #print(result_string)
-    assert result == calendar1
-    #CURRENTLY----------------------DOES-------------------------NOT------------------------------WORK-----------------------------------------------
-    """
-
-def test2():
-    #test will just print calendar to the console for this range
-    #currently failing because it claims it takes 3 positional arguments
-
-    #use one of the above ids for userid, this is Jared's
-    userid = "auth0|5cb50516ee4bd5113d54872b" 
-     # year-month-day(T)two digit hours: two digit minutes: two digit seconds(-04 is subtracted from UTC, this for example is EST eastern time) 
-    start = "2019-04-15T00:00:00-04:00"
-    end = "2019-04-21T00:00:00-04:00"
-    timezone = "America/New York" #keep this the same...we can't test other timezones rn
     result = calendar.get_calendar(userid, start, end, timezone)
-    #""""
-    result_string = ""
-    for event in result:
-       result_string += event.starttime + ' - ' + event.endtime + '\n'
-    print(result_string)
-    #""""
+    assert result == correct_calendar
 
 
 if __name__ == '__main__':
-    test2()
+    test1()
     print("Passed all tests.")
-
