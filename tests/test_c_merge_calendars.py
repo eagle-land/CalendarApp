8a8b466c0e0f84934fc45ce7c887db7b536502b0
sardonicphoenix <37968570+sardonicphoenix@users.noreply.github.com>
2019-04-24 14:24:40 -0400

    Update test_c_merge_calendars.py
    added two more tests

--- tests/test_c_merge_calendars.py
+++ tests/test_c_merge_calendars.py
@@ -70,10 +70,56 @@ def test3():
     mergedcalendar = calendar.Calendar(mergedevents)  # compare calendar variable
     result = calendar.merge_calendars(calendar1, calendar2)  # test variables compare
     assert result == mergedcalendar  # assert will say whether the test failed or not
+def test4():
+    # Example calendar 1
+    user1events = [
+        calendar.Event("2019-05-16T13:00:00-04:00", "2019-05-16T20:00:00-04:00")
+        # year-month-day(T)hours:minutes:seconds(-04 is subtracted from UTC this is EST
+    ]
+    calendar1 = calendar.Calendar(user1events)  # put user1 events into calendar
 
+    # second user calendar events defined
+    user2events = [
+        calendar.Event("2019-05-16T08:00:00-04:00", "2019-05-16T22:00:00-04:00")
+    ]
+    calendar2 = calendar.Calendar(user2events)  # put user2 events into calendar
+
+    # array with correct variables for compare
+    mergedevents = [
+        calendar.Event("2019-05-16T08:00:00-04:00", "2019-05-16T22:00:00-04:00")
+    ]
+
+    mergedcalendar = calendar.Calendar(mergedevents)  # compare calendar variable
+    result = calendar.merge_calendars(calendar1, calendar2)  # test variables compare
+    assert result == mergedcalendar  # assert will say whether the test failed or not
+
+def test5():
+    # Example calendar 1
+    user1events = [
+        calendar.Event("2019-05-15T10:00:00-04:00", "2019-05-15T20:00:00-04:00")
+        # year-month-day(T)hours:minutes:seconds(-04 is subtracted from UTC this is EST
+    ]
+    calendar1 = calendar.Calendar(user1events)  # put user1 events into calendar
+
+    # second user calendar events defined
+    user2events = [
+        calendar.Event("2019-05-15T10:00:00-04:00", "2019-05-15T22:00:00-04:00")
+    ]
+    calendar2 = calendar.Calendar(user2events)  # put user2 events into calendar
+
+    # array with correct variables for compare
+    mergedevents = [
+        calendar.Event("2019-05-15T10:00:00-04:00", "2019-05-15T22:00:00-04:00")
+    ]
+
+    mergedcalendar = calendar.Calendar(mergedevents)  # compare calendar variable
+    result = calendar.merge_calendars(calendar1, calendar2)  # test variables compare
+    assert result == mergedcalendar  # assert will say whether the test failed or not
 
 if __name__ == '__main__':
     test1()
     test2()
     test3()
+    test4()
+    test5()
     print("Passed all tests.")
